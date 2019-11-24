3e7dcf3eb237825b79bda0d4633eb9c8bc90121a
calebdeji <calebdeji06@gmail.com>
2019-11-24 06:52:40 +0100

    ignore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,87 @@
-import React, {	Component } from 'react';
+import React, { Component } from 'react';
+import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
 import './App.css';
+import Logo from './logo.png';
+import Home  from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faPhoneAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 
-class App extends Component {
-	constructor(props) {
+
+
+export default class App extends Component{
+	constructor(props){
 		super(props);
-		this.props = props;
-	}
-	render() {
-		return (
-			<p>Hello Everyone</p>
-		 );
+        this.props = props;
+        this.state = {navText:false, navTextDisplay : 'none'};
+    }
+
+
+    /**
+     * to detects the screen size and decide the view of nav-text
+     */
+    componentDidMount(){
+        window.addEventListener("resize",this.handleNavDisplay);
+        this.handleNavDisplay()
+    }
+
+    handleNavDisplay = () => {
+        const screenSize = window.innerWidth;
+        if(screenSize > 500){
+            this.setState( ( state ) => ({
+                navText : !state.navText,
+                navTextDisplay : 'flex'
+            }));
+        }
+        else{
+            this.setState( ( state ) => ({
+                navText : !state.navText,
+                navTextDisplay : 'none'
+            }));
+        }
+    }
+
+    /**
+     * method called when the bar icon is clicked
+     */
+    handleBarClick = (event) =>{
+
+        event.preventDefault();
+
+        if(this.state.navText){
+            this.setState( ( state ) => ({
+                navText : !state.navText,
+                navTextDisplay : 'none'
+            }));
+        }
+        else{
+            this.setState( ( state ) => ({
+                navText : !state.navText,
+                navTextDisplay : 'flex'
+            }));
+        }
+    }
+
+	render(){
+		return(
+			<Router>
+				<nav className ="nav">
+                    <Link to='/pages/Home/Home' className="nav__nav-link-logo link"><img src={Logo} className="nav__nav-logo" alt="A figure that depicts school"></img></Link>
+                    <button onClick={this.handleBarClick} className="nav__nav-bar-icon link"><FontAwesomeIcon icon={faBars} /></button>
+                    <div className="nav__text-and-icon" style={{display : this.state.navTextDisplay}}>
+                        <Link to = 'pages/Home/Home' className="nav__text link"><FontAwesomeIcon icon={faHome} className="icon" />  Home </Link>
+                        <Link to = '/pages/Contact/Contact' className="nav__text link"><FontAwesomeIcon icon={faPhoneAlt} className="icon"/>  Contact the Dev</Link>
+                    </div>
+                </nav>
+                <Switch>
+                    <Route exact path='/pages/Home' component={Home}></Route>
+                    <Route exact path='/pages/Home' component={Home}></Route>
+                    <Route path='pages/Contact' component={Contact}></Route>
+                    <Route component={NotFoundPage}></Route>
+                </Switch>
+			</Router>
+		);
 	}
 }
 
-export default App;
