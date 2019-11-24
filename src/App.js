c5df1b4037a1b80ddf0d0c1e1e71c29dda93e7fc
calebdeji <calebdeji06@gmail.com>
2019-11-24 15:10:05 +0100

    changed routing from index.js file to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
 import './App.css';
 import Logo from './logo.png';
 import Home  from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
+import Footer from './component/Footer/Footer';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faPhoneAlt, faBars } from '@fortawesome/free-solid-svg-icons';
@@ -36,7 +37,7 @@ export default class App extends Component{
         }
         else{
             this.setState( ( state ) => ({
-                navText : !state.navText,
+                navText : false,
                 navTextDisplay : 'none'
             }));
         }
@@ -48,7 +49,7 @@ export default class App extends Component{
     handleBarClick = (event) =>{
 
         event.preventDefault();
-
+        console.log("state status is : ", this.state);
         if(this.state.navText){
             this.setState( ( state ) => ({
                 navText : !state.navText,
@@ -65,22 +66,25 @@ export default class App extends Component{
 
 	render(){
 		return(
-			<Router>
-				<nav className ="nav">
-                    <Link to='/pages/Home/Home' className="nav__nav-link-logo link"><img src={Logo} className="nav__nav-logo" alt="A figure that depicts school"></img></Link>
-                    <button onClick={this.handleBarClick} className="nav__nav-bar-icon link"><FontAwesomeIcon icon={faBars} /></button>
-                    <div className="nav__text-and-icon" style={{display : this.state.navTextDisplay}}>
-                        <Link to = 'pages/Home/Home' className="nav__text link"><FontAwesomeIcon icon={faHome} className="icon" />  Home </Link>
-                        <Link to = '/pages/Contact/Contact' className="nav__text link"><FontAwesomeIcon icon={faPhoneAlt} className="icon"/>  Contact the Dev</Link>
-                    </div>
-                </nav>
-                <Switch>
-                    <Route exact path='/pages/Home' component={Home}></Route>
-                    <Route exact path='/pages/Home' component={Home}></Route>
-                    <Route path='pages/Contact' component={Contact}></Route>
-                    <Route component={NotFoundPage}></Route>
-                </Switch>
-			</Router>
+			<Fragment>
+                <Router>
+                    <nav className ="nav">
+                        <Link to='/pages/Home/Home' className="nav__nav-link-logo link"><img src={Logo} className="nav__nav-logo" alt="A figure that depicts school"></img></Link>
+                        <button onClick={this.handleBarClick} className="nav__nav-bar-icon link"><FontAwesomeIcon icon={faBars} /></button>
+                        <div className="nav__text-and-icon" style={{display : this.state.navTextDisplay}}>
+                            <Link to = '/pages/Home/Home' className="nav__text link"><FontAwesomeIcon icon={faHome} className="icon" />  Home </Link>
+                            <Link to = '/pages/Contact/Contact' className="nav__text link"><FontAwesomeIcon icon={faPhoneAlt} className="icon"/>  Contact the Dev</Link>
+                        </div>
+                    </nav>
+                    <Switch>
+                        <Route path='/pages/Home' component = { Home } />
+                        <Route path='/pages/Home' component = { Home } />
+                        <Route path='/pages/Contact' component = { Contact } />
+                        <Route component={NotFoundPage}/>
+                    </Switch>
+                </Router>
+                <Footer />
+            </Fragment>
 		);
 	}
 }
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <p className="footer-text">
+                Developed by <a href="https://calebdeji.tech">Calebdeji</a>
+            </p>
+        )
+    }
+}
