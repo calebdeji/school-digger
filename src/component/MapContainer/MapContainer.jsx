b5b4523b7fe1675c756ff4a3f5e6ccce45e8235a
calebdeji <calebdeji06@gmail.com>
2020-05-31 07:02:29 +0100

    Render Marker on google map

--- src/component/MapContainer/MapContainer.jsx
+++ src/component/MapContainer/MapContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { CircularProgress } from "@material-ui/core";
 
 export class MapContainer extends Component {
     constructor(props) {
@@ -7,6 +8,7 @@ export class MapContainer extends Component {
         this.props = props;
     }
     render() {
+        console.log({ props: this.props });
         return (
             <Map
                 google={this.props.google}
@@ -16,13 +18,16 @@ export class MapContainer extends Component {
                     width: "100%",
                     height: "500px",
                     position: "static",
-                    margin: "auto"
+                    margin: "auto",
                 }}
                 initialCenter={{ lat: this.props.lat, lng: this.props.lng }}
-            />
+            >
+                <Marker />
+            </Map>
         );
     }
 }
 export default GoogleApiWrapper({
-    apiKey: process.env.REACT_APP_GoogleAPIKey
+    apiKey: process.env.REACT_APP_GoogleAPIKey,
+    LoadingContainer: CircularProgress,
 })(MapContainer);
